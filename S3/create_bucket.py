04d2dd0e78239200db4f030c8257d6fb902e198f
IrinaA <irina.o.andrei@gmail.com>
2023-10-25 19:03:56 +0100

    Added new provisions for app and db

--- S3/create_bucket.py
+++ S3/create_bucket.py
@@ -1,7 +1,7 @@
 import boto3
 
 s3 = boto3.client('s3')
-# this stores the connection to aws
+# this stores the connection to AWS
 
 s3.create_bucket(Bucket='tech254-irina-bucket-test',
                 CreateBucketConfiguration={
@@ -10,4 +10,9 @@ s3.create_bucket(Bucket='tech254-irina-bucket-test',
 # creating the bucket
 
 print("Creation of bucket was successful.")
-# printing back an output message to show the creation was successful.
+# printing back an output message to show the process was a success.
+
+
+
+
+
